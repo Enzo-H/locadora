8315a9138d0229f3ac12be90314dc567dfc40384
Enzo Hermes <enzohermes85@gmail.com>
2020-12-03 00:25:07 -0200

    commit clienteDao

---
+++ src/model/dao/ClienteDAO.java
@@ -0,0 +1,70 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import connection.ConnectionFactory;
+import model.bean.Cliente;
+
+public class ClienteDAO { 
+
+	public void create(Cliente c) {
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement stmt = null;
+
+	try {
+		stmt = con.prepareStatement("INSERT INTO CLIENTE (nome, email, sexo) VALUES"
+					+ "(?,?,?)");
+		
+		stmt.setString(1, c.getNome());
+		stmt.setString(2, c.getEmail());
+		stmt.setBoolean(3, c.isSexo());
+
+		stmt.executeUpdate();
+		
+			JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso!");
+		} catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao cadastrar cliente: "+ e);
+		}finally{
+			ConnectionFactory.closeConnection(con, stmt);
+		}
+	
+	}
+
+	public List<Cliente> read() {
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<Cliente> clientes = new ArrayList<>();
+
+		try {
+			
+		stmt = con.prepareStatement("SELECT * FROM cliente;");
+		rs = stmt.executeQuery();
+		while (rs.next()) {
+			
+			Cliente c = new Cliente();
+			c.setIdCliente(rs.getInt("idCliente"));
+			c.setNome(rs.getString("nome"));
+			c.setEmail(rs.getString("email"));
+			c.setSexo(rs.getBoolean("sexo"));
+			clientes.add(c);
+			}
+		
+		} catch (SQLException e) {
+		JOptionPane.showMessageDialog(null, "Erro ao buscar as informações do BD: " + e);
+		e.printStackTrace();
+		} finally {
+			ConnectionFactory.closeConnection(con, stmt, rs);
+		}
+		
+		return clientes;
+	}
+
+}
